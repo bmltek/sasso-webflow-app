3dcd638a72b861718b666dc88914d324b3e89dd8
Bolanle Longe <bolatolulope@gmail.com>
2025-04-01 17:57:37 +0100

    edited

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,22 @@
-resource "azurerm_resource_group" "aks" {
-  name     = "${var.prefix}-rg"
-  location = var.location
+resource "azurerm_resource_group" "tfstate" {
+  name     = "sasso-tfstate-rg"
+  location = "westeurope"
+  tags = {
+    environment = "dev"
+    project     = "sasso-webflow"
+  }
 }
 
-resource "random_string" "acr_suffix" {
+resource "random_string" "storage_account" {
   length  = 8
   special = false
   upper   = false
 }
 
-resource "azurerm_container_registry" "acr" {
-  name                = "sasso${random_string.acr_suffix.result}acr"
-  resource_group_name = azurerm_resource_group.aks.name
-  location            = azurerm_resource_group.aks.location
-  sku                 = "Standard"
-  admin_enabled       = true
-  tags                = var.tags
-}
-
 resource "azurerm_storage_account" "tfstate" {
-  name                     = "tfstate${random_string.acr_suffix.result}"
-  resource_group_name      = azurerm_resource_group.aks.name
-  location                 = azurerm_resource_group.aks.location
+  name                     = "tfstate${random_string.storage_account.result}"
+  resource_group_name      = azurerm_resource_group.tfstate.name
+  location                 = azurerm_resource_group.tfstate.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   min_tls_version         = "TLS1_2"
@@ -30,102 +25,14 @@ resource "azurerm_storage_account" "tfstate" {
     versioning_enabled = true
   }
 
-  tags = var.tags
+  tags = {
+    environment = "dev"
+    project     = "sasso-webflow"
+  }
 }
 
 resource "azurerm_storage_container" "tfstate" {
   name                  = "tfstate"
   storage_account_name  = azurerm_storage_account.tfstate.name
   container_access_type = "private"
-}
-
-resource "azurerm_log_analytics_workspace" "aks" {
-  name                = "${var.prefix}-law"
-  location            = azurerm_resource_group.aks.location
-  resource_group_name = azurerm_resource_group.aks.name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
-}
-
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "${var.prefix}-aks"
-  location            = azurerm_resource_group.aks.location
-  resource_group_name = azurerm_resource_group.aks.name
-  dns_prefix          = "${var.prefix}-aks"
-  kubernetes_version  = var.kubernetes_version
-
-  default_node_pool {
-    name                = "default"
-    node_count          = var.node_count
-    vm_size             = var.vm_size
-    enable_auto_scaling = true
-    min_count           = var.min_node_count
-    max_count           = var.max_node_count
-    os_disk_size_gb     = 50
-    type                = "VirtualMachineScaleSets"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  network_profile {
-    network_plugin    = "azure"
-    load_balancer_sku = "standard"
-  }
-
-  auto_scaler_profile {
-    scale_down_delay_after_add = "15m"
-    scale_down_unneeded        = "15m"
-  }
-
-  tags = var.tags
-}
-
-resource "azurerm_monitor_diagnostic_setting" "aks" {
-  name                       = "aks-diagnostics"
-  target_resource_id         = azurerm_kubernetes_cluster.aks.id
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.aks.id
-
-  enabled_log {
-    category = "kube-apiserver"
-  }
-
-  enabled_log {
-    category = "kube-audit"
-  }
-
-  enabled_log {
-    category = "kube-controller-manager"
-  }
-
-  enabled_log {
-    category = "kube-scheduler"
-  }
-
-  enabled_log {
-    category = "cluster-autoscaler"
-  }
-
-  metric {
-    category = "AllMetrics"
-    enabled  = true
-  }
-
-  depends_on = [azurerm_kubernetes_cluster.aks, azurerm_log_analytics_workspace.aks]
-}
-
-resource "azurerm_role_assignment" "aks_acr" {
-  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-  role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.acr.id
-  skip_service_principal_aad_check = true
-  depends_on                       = [azurerm_kubernetes_cluster.aks, azurerm_container_registry.acr]
-}
-
-resource "azurerm_role_assignment" "aks_acr_pull" {
-  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
-  role_definition_name = "AcrPull"
-  scope                = azurerm_container_registry.acr.id
-  depends_on           = [azurerm_kubernetes_cluster.aks, azurerm_container_registry.acr]
 }
