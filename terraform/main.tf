9c301757a3c715adf13f3dfe270c91f7177e5a8a
Bolanle Longe <bolatolulope@gmail.com>
2025-04-01 15:00:04 +0100

    update terraform code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-  
-  backend "azurerm" {
-    # Backend will be configured via Azure Pipeline
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "aks" {
   name     = "${var.prefix}-rg"
   location = var.location
