d067f8e52d0992e36a533149b0417fdb9d937e74
Bolanle Longe <bolatolulope@gmail.com>
2025-04-01 18:50:52 +0100

    update

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,9 +5,15 @@ terraform {
       version = "~> 3.0"
     }
   }
+  
+  backend "azurerm" {
+    resource_group_name  = "sasso-tfstate-rg"
+    storage_account_name = "tfstat56eterraform45"
+    container_name       = "tfstate"
+    key                 = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
   features {}
 }
-
