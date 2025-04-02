8cc5477a9b98159ef9145e8a31b9c30d8fb917e0
Bolanle Longe <bolatolulope@gmail.com>
2025-04-02 11:57:16 +0100

    update

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -11,10 +11,6 @@ terraform {
     storage_account_name = "tfstat56eterraform45"
     container_name       = "tfstate"
     key                 = "terraform.tfstate"
-    subscription_id     = "a14b6cfe-8553-4b23-b354-d5602f0f84aa"
-    use_oidc            = true
-    use_azuread_auth    = true
-    tenant_id           = "72f988bf-86f1-41af-91ab-2d7cd011db47"
   }
 }
 
