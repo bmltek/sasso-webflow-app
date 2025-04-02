f08fc375a7f298f506081e998f4699e1901022fe
Bolanle Longe <bolatolulope@gmail.com>
2025-04-02 11:52:13 +0100

    updated terraform

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -11,6 +11,10 @@ terraform {
     storage_account_name = "tfstat56eterraform45"
     container_name       = "tfstate"
     key                 = "terraform.tfstate"
+    subscription_id     = "a14b6cfe-8553-4b23-b354-d5602f0f84aa"
+    use_oidc            = true
+    use_azuread_auth    = true
+    tenant_id           = "72f988bf-86f1-41af-91ab-2d7cd011db47"
   }
 }
 
@@ -20,4 +24,5 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
+  subscription_id = "a14b6cfe-8553-4b23-b354-d5602f0f84aa"
 }
