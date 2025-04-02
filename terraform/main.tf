1a1d9ef31fffb175c225404a5ea8a9d01b9e50cf
Bolanle Longe <bolatolulope@gmail.com>
2025-04-02 12:01:43 +0100

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -101,29 +101,8 @@ resource "azurerm_monitor_diagnostic_setting" "aks" {
   target_resource_id        = azurerm_kubernetes_cluster.aks.id
   log_analytics_workspace_id = azurerm_log_analytics_workspace.aks.id
 
-  log {
-    category = "kube-apiserver"
-    enabled  = true
-  }
-
-  log {
-    category = "kube-controller-manager"
-    enabled  = true
-  }
-
-  log {
-    category = "kube-scheduler"
-    enabled  = true
-  }
-
-  log {
-    category = "kube-audit"
-    enabled  = true
-  }
-
-  log {
-    category = "cluster-autoscaler"
-    enabled  = true
+  enabled_log {
+    category_group = "allLogs"
   }
 
   metric {
